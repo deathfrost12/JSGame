18258b0ccf25fb38ca93a86be81fe4f1e867573e
deathfrost12 <98310677+deathfrost12@users.noreply.github.com>
2023-01-19 20:43:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,12 +16,30 @@ let options = {
         "Pineapple",
         "Pomegranate",
         "Watermelon",
+        "Cherry",
+        "Papaya",
+        "Cucumber",
+        "Mango",
+        "Dragonfruit",
+        "Coconut",
+        "Avocado",
+        "Pear",
+        "Grape",
     ],
     animals: ["Hedgehog",
         "Rhinoceros",
         "Squirrel",
         "Panther",
         "Walrus",
+        "Whale",
+        "Turkey",
+        "Seahorse",
+        "Monkey",
+        "Turtle",
+        "Hippo",
+        "Wolf",
+        "Shark",
+        "Aligator",
         "Zebra"],
     countries: [
         "India",
@@ -30,6 +48,15 @@ let options = {
         "Switzerland",
         "Zimbabwe",
         "Dominica",
+        "Panama",
+        "Romania",
+        "Croatia",
+        "Pakistan",
+        "Australia",
+        "Albania",
+        "Denmark",
+        "Syria",
+        "Singapore",
     ],
 };
 
@@ -156,4 +183,90 @@ const initializer = () => {
     initialDrawing();
 };
 
+//plátno
+const canvasCreator = () => {
+    let context = canvas.getContext("2d");
+    context.beginPath();
+    context.strokeStyle = "#000";
+    context.lineWidth = 2;
+
+    //kreslení čar pro vykreslení postavičky
+    const drawLine = (fromX, fromY, toX, toY) => {
+        context.moveTo(fromX, fromY);
+        context.lineTo(toX, toY);
+        context.stroke();
+    };
+
+    const head = () => {
+        context.beginPath();
+        context.arc(70, 30, 10, 0, Math.PI * 2, true);
+        context.stroke();
+    };
+
+    const body = () => {
+        drawLine(70, 40, 70, 80);
+    };
+
+    const leftArm = () => {
+        drawLine(70, 50, 50, 70);
+    };
+
+    const rightArm = () => {
+        drawLine(70, 50, 90, 70);
+    };
+
+    const leftLeg = () => {
+        drawLine(70, 80, 50, 110);
+    };
+
+    const rightLeg = () => {
+        drawLine(70, 80, 90, 110);
+    };
+
+    //počáteční vizáž (šibenice)
+    const initialDrawing = () => {
+        //vymazaní plátna
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        //spodní čára
+        drawLine(10, 130, 130, 130);
+        //levá čára
+        drawLine(10, 10, 10, 131);
+        //horní čára
+        drawLine(10, 10, 70, 10);
+        //malá horní čára
+        drawLine(70, 10, 70, 20);
+    };
+
+    return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
+};
+
+//vykreslení šibenice
+const drawMan = (count) => {
+    let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
+    switch (count) {
+        case 1:
+            head();
+            break;
+        case 2:
+            body();
+            break;
+        case 3:
+            leftArm();
+            break;
+        case 4:
+            rightArm();
+            break;
+        case 5:
+            leftLeg();
+            break;
+        case 6:
+            rightLeg();
+            break;
+        default:
+            break;
+    }
+};
+
+//nová hra
+newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
